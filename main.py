440cd4c6fa7609fb7263b870c593453b0a541032
ZizaKariuki <zizawabetti@gmail.com>
2026-08-23 18:24:43 +0300

    Implimenting select filtering

--- main.py
+++ main.py
@@ -1,39 +1,75 @@
 # STEP 1A
 # Import SQL Library and Pandas
+import sqlite3 
+import pandas as pd
 
 # STEP 1B
 # Connect to the database
-conn = None
+conn = sqlite3.connect('data.sqlite')
 
 
 # STEP 2
 # Replace None with your code
-df_first_five = None
+df_first_five = pd.read_sql("""
+SELECT employeeNumber,lastName FROM employees;
+""", conn)
 
 # STEP 3
 # Replace None with your code
-df_five_reverse = None
+df_five_reverse = pd.read_sql("""
+SELECT lastName,employeeNumber FROM employees;
+""", conn)
+
 
 # STEP 4
 # Replace None with your code
-df_alias = None
+df_alias = pd.read_sql("""
+SELECT employeeNumber AS ID FROM employees;
+""", conn)
+
 
 # STEP 5
 # Replace None with your code
-df_executive = None
+df_executive = pd.read_sql("""
+SELECT jobTitle,
+CASE
+    WHEN jobTitle = 'President' OR jobTitle = 'VP Sales' OR jobTitle = 'VP Marketing' THEN 'Executive'
+    ELSE 'Not Executive'
+END AS role
+FROM employees;
+""", conn)
+
 
 # STEP 6
 # Replace None with your code
-df_name_length = None
+df_name_length = pd.read_sql("""
+SELECT LENGTH(lastName) AS name_length FROM employees;
+""", conn)
 
 # STEP 7
 # Replace None with your code
-df_short_title = None
+df_short_title =  pd.read_sql("""
+SELECT SUBSTR(jobTitle, 1,2) AS short_title FROM employees;
+""", conn)
+
 
 # STEP 8
 # Replace None with your code
-sum_total_price = None
+sum_total_price = pd.read_sql("""
+SELECT ROUND(priceEach * quantityOrdered, 2) AS total_price
+FROM orderDetails;
+""", conn).sum()
 
 # STEP 9
 # Replace None with your code
-df_day_month_year = None
+df_day_month_year = pd.read_sql("""
+SELECT orderDate,
+strftime('%d', orderDate) AS day,
+strftime('%m', orderDate) AS month,
+strftime('%Y', orderDate) AS year
+FROM orders;
+""", conn)
+
+print(df_day_month_year)
+
+conn.close()
